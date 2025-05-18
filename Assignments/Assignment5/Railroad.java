3c9672d2c1b1d34dea2973e724aed780481a167c
Quil <quil180gaming@gmail.com>
2025-05-18 19:05:14 -0400

    course over

--- Assignments/Assignment5/Railroad.java
+++ Assignments/Assignment5/Railroad.java
@@ -1,6 +1,6 @@
 /* Yousef Alaa Awad
  * Dr. Steinberg
- * +COP3503 Spring 2025
+ * COP3503 Spring 2025
  * Programming Assignment 5
  */
 
@@ -11,30 +11,33 @@ import java.util.Scanner;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Railroad 
+public class Railroad
 {
+  // each railroad track has a cost, source, and destination
   private int cost;
   private String source;
   private String destination;
 
-  private static Railroad[] allRails; 
+  // array of all rails
+  private static Railroad[] allRails;
 
+  // first and only public constructor
   public Railroad(int tracks, String inputFile)
   {
-    // ok so this clears all the allRails
+    // clear previous rails
     allRails = new Railroad[tracks];
 
-    // file reading
+    // read input file
     try (Scanner scanner = new Scanner(new File(inputFile)))
     {
-      // only getting the correct amount of tracks in the file
+      // read specified number of tracks
       for (int i = 0; i < tracks; i++)
       {
         String src = scanner.next();
         String dest = scanner.next();
         int cost = scanner.nextInt();
 
-        // putting them into the array
+        // add rail to array
         allRails[i] = new Railroad(src, dest, cost);
       }
     }
@@ -44,141 +47,160 @@ public class Railroad
     }
   }
 
-  // second constructor for populating array of all tracks
-  public Railroad(String source, String destination, int cost) 
+  // private constructor for rail entries
+  private Railroad(String source, String destination, int cost)
   {
     this.source = source;
     this.destination = destination;
     this.cost = cost;
   }
 
-  public String buildRailroad() 
+  // kruskal wrapper for finding optimal tracks
+  public String buildRailroad()
   {
-    // sorts the railways by cost
+    // sort rails by ascending cost
     sortRailways();
-    
-    // this runs kruskal's to find minimum spanning tree
+
+    // apply kruskal's algorithm to find minimum spanning tree
     Railroad[] mstEdges = kruskal();
-    
-    // constructs the output string
+
+    // build output string
     StringBuilder result = new StringBuilder();
     int totalCost = 0;
-    
-    for (Railroad rail : mstEdges) {
-      if (rail != null) {
+
+    // add each rail in MST to output
+    for (Railroad rail : mstEdges)
+    {
+      if (rail != null)
+      {
         result.append(printOutput(rail.source, rail.destination, rail.cost));
         totalCost += rail.cost;
       }
     }
-    
+
+    // add total cost to output
     result.append("The cost of the railroad is $").append(totalCost).append(".");
+
     return result.toString();
   }
-  
-  private void sortRailways() 
+
+  private void sortRailways()
   {
-    // sorts the allRails array by cost in ascending order
+    // sort allRails by cost ascending
     Arrays.sort(allRails, (a, b) -> Integer.compare(a.cost, b.cost));
   }
 
   private String printOutput(String track1, String track2, int cost)
   {
-    return track1 + "---" + track2 + "\t$" + cost + "\n";
+    // generate ordered rail output
+    String a = track1;
+    String b = track2;
+
+    // swap if out of order
+    if (a.compareTo(b) > 0)
+    {
+      a = track2;
+      b = track1;
+    }
+
+    return a + "---" + b + "\t$" + cost + "\n";
   }
 
+  // apply kruskal's algorithm to compute MST
   private Railroad[] kruskal()
   {
-    // this will help find all unique vertices
+    // map vertex names to indices
     Map<String, Integer> vertices = new HashMap<>();
     int vertexCount = 0;
-    
-    for (Railroad rail : allRails) 
+
+    // register vertices
+    for (Railroad rail : allRails)
     {
       if (!vertices.containsKey(rail.source))
       {
+        // add source if new
         vertices.put(rail.source, vertexCount++);
       }
-      if (!vertices.containsKey(rail.destination)) 
+      if (!vertices.containsKey(rail.destination))
       {
+        // add destination if new
         vertices.put(rail.destination, vertexCount++);
       }
     }
-    
-    // intializes disjoint set for all vertices
+
     DisjointSet ds = new DisjointSet(vertexCount);
-    
-    // Array to store minimum spanning tree edges
     Railroad[] mstEdges = new Railroad[vertexCount - 1];
     int mstEdgeCount = 0;
-    
-    // applying kruskals
-    for (Railroad rail : allRails) 
+
+    // select edges without creating cycles
+    for (Railroad rail : allRails)
     {
       int sourceRoot = ds.find(vertices.get(rail.source));
       int destRoot = ds.find(vertices.get(rail.destination));
-      
-      // if including this edge doesn't form a cycle, add it to MST
-      if (sourceRoot != destRoot)   
+
+      // add edge if no cycle
+      if (sourceRoot != destRoot)
       {
         mstEdges[mstEdgeCount++] = rail;
         ds.union(sourceRoot, destRoot);
-        
-        // if we have n-1 edges, MST is complete
-        if (mstEdgeCount == vertexCount - 1) 
+
+        // stop when MST complete
+        if (mstEdgeCount == vertexCount - 1)
         {
           break;
         }
       }
     }
-    
+
     return mstEdges;
   }
-  
-  private static class DisjointSet 
+
+  // disjoint-set for union-find operations
+  private static class DisjointSet
   {
     private int[] parent;
     private int[] rank;
-    
-    public DisjointSet(int size)  
+
+    public DisjointSet(int size)
     {
       parent = new int[size];
       rank = new int[size];
-      
-      for (int i = 0; i < size; i++) 
+
+      for (int i = 0; i < size; i++)
       {
         parent[i] = i;
         rank[i] = 0;
       }
     }
-    
-    public int find(int x) 
+
+    public int find(int x)
     {
-      if (parent[x] != x) 
+      if (parent[x] != x)
       {
         parent[x] = find(parent[x]);
       }
       return parent[x];
     }
-    
-    // union by rank
-    public void union(int x, int y) 
+
+    public void union(int x, int y)
     {
       int rootX = find(x);
       int rootY = find(y);
-      
-      if (rootX == rootY) 
+
+      if (rootX == rootY)
       {
         return;
       }
-      
-      // helps keep the tree balanced vro
-      if (rank[rootX] < rank[rootY]) 
+
+      if (rank[rootX] < rank[rootY])
       {
         parent[rootX] = rootY;
-      } else if (rank[rootX] > rank[rootY]) 
+      }
+      else if (rank[rootX] > rank[rootY])
       {
         parent[rootY] = rootX;
-      } else 
+      }
+      else
       {
         parent[rootY] = rootX;
         rank[rootX]++;
@@ -186,4 +208,3 @@ public class Railroad
     }
   }
 }
-  
